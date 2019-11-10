a762b626fd905a5f92c5c7e5142a8fda38389c49
Eldar Sieitablaiev <eldar.seytablaev@gmail.com>
2019-11-10 11:38:49 +0200

    travis-yml v13

--- scripts/open_pull_request.sh
+++ scripts/open_pull_request.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#https://#' -e 's@com:@com/@' -e 's%\.git$%%' | awk '/github/'`;
-branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
-pr_url=$github_url"/compare/master..."$branch_name
-open $pr_url;
+function openpr() {
+  github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#https://#' -e 's@com:@com/@' -e 's%\.git$%%' | awk '/github/'`;
+  branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
+  pr_url=$github_url"/compare/master..."$branch_name
+  open $pr_url;
+}
+
+git push origin HEAD
+
+if [ $? -eq 0 ]; then
+  openpr
+else
+  echo 'failed to push commits and open a pull request.';
+fi
