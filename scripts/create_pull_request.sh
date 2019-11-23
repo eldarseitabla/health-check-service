2286facf91d3b9ad08124bd15b894a452351abc8
Eldar Sieitablaiev <eldar.seytablaev@gmail.com>
2019-11-23 23:54:47 +0200

    Pull-request only feature branch

--- scripts/create_pull_request.sh
+++ scripts/create_pull_request.sh
@@ -8,33 +8,34 @@ EOF
 }
 
 function createPullRequest() {
-  remote_url=`git config remote.origin.url`;
-
-  remote_url=${remote_url//https:\/\//}; # remove https://
-  remote_url=${remote_url//git@github\.com\:/}; # remove git@github.com
-  remote_url=${remote_url//github\.com\//}; # remove github.com
-  username=`echo $remote_url | sed -e 's/\/.*$//g'`; # remove repo name
-  repository_name=`basename -s .git $remote_url`;
   branch_name=$TRAVIS_BRANCH;
-  title_prepare=`echo $branch_name | tr "/" " "`;
-  title=`echo ${title_prepare:0:1} | tr '[a-z]' '[A-Z]'`${title_prepare:1};
-  body=`git rev-list --simplify-by-decoration -2 $branch_name --pretty=format:"%h - %s"`;
-  body=${body//\\/\\\\}; # \
-  body=${body//\//\\\/}; # /
-  body=${body//\'/\\\'}; # ' (not strictly needed ?)
-  body=${body//\"/\\\"}; # "
-  body=${body//   /\\t}; # \t (tab)
-  body=${body//
+  if [[ "$branch_name" =~ ^"feature/".* ]]; then
+    remote_url=`git config remote.origin.url`;
+    remote_url=${remote_url//https:\/\//}; # remove https://
+    remote_url=${remote_url//git@github\.com\:/}; # remove git@github.com
+    remote_url=${remote_url//github\.com\//}; # remove github.com
+    username=`echo $remote_url | sed -e 's/\/.*$//g'`; # remove repo name
+    repository_name=`basename -s .git $remote_url`;
+    title_prepare=`echo $branch_name | tr "/" " "`;
+    title=`echo ${title_prepare:0:1} | tr '[a-z]' '[A-Z]'`${title_prepare:1};
+    body=`git rev-list --simplify-by-decoration -2 $branch_name --pretty=format:"%h - %s"`;
+    body=${body//\\/\\\\}; # \
+    body=${body//\//\\\/}; # /
+    body=${body//\'/\\\'}; # ' (not strictly needed ?)
+    body=${body//\"/\\\"}; # "
+    body=${body//   /\\t}; # \t (tab)
+    body=${body//
 /\\\n}; # \n (newline)
-  body=${body//^M/\\\r}; # \r (carriage return)
-  body=${body//^L/\\\f}; # \f (form feed)
-  body=${body//^H/\\\b}; # \b (backspace)
+    body=${body//^M/\\\r}; # \r (carriage return)
+    body=${body//^L/\\\f}; # \f (form feed)
+    body=${body//^H/\\\b}; # \b (backspace)
 
-  curl -i \
-  -H "Authorization: token $GITHUB_TOKEN" \
-  -H "Content-Type: application/json" \
-  -X POST --data "$(get_json_data)" \
-  "https://api.github.com/repos/$username/$repository_name/pulls"
+    curl -i \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    -H "Content-Type: application/json" \
+    -X POST --data "$(get_json_data)" \
+    "https://api.github.com/repos/$username/$repository_name/pulls"
+  fi
 }
 
 if [ $? -eq 0 ]; then
